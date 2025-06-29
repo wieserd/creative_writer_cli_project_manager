1f21fa73ec8139eba36bf3fda92b25edc755a3ce
Daniel <35579566+wieserd@users.noreply.github.com>
2025-06-29 17:22:21 +0200

    Update cli.py

--- src/creative_writer_cli/ui/cli.py
+++ src/creative_writer_cli/ui/cli.py
@@ -119,7 +119,7 @@ class CLI:
                 project_meta = self.project_repository.get_project_meta(project_name)
                 project_type = project_meta.get('type')
                 sections = self.project_repository.get_project_sections(project_name)
-                self.project_overview(project_name, sections, project_type)
+                display.project_overview(project_name, sections, project_type)
             elif choice == "Export Project":
                 self.export_project(project_name)
             elif choice == "Back to Main Menu" or choice is None:
