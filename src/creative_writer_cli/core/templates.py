ef196f9e8e3cc064f8dc5a3bfea70a1acb641638
Daniel <35579566+wieserd@users.noreply.github.com>
2025-06-30 19:26:33 +0200

    Update templates.py

--- src/creative_writer_cli/core/templates.py
+++ src/creative_writer_cli/core/templates.py
@@ -3,5 +3,5 @@ TEMPLATES = {
     "Poem": ["Theme", "Stanzas", "Imagery", "Notes"],
     "Scientific Article": ["Title", "Abstract", "Introduction", "Methods", "Results", "Discussion", "References"],
     "Scientific Book": ["Title", "Abstract", "Introduction", "Chapter 1", "Chapter 2", "Chapter 3", "Conclusion", "References"],
-    "Other": ["Notes"]
+    "General Notes": ["Notes"]
 }
